4f575a7ad4bd2fffd70a1dcfa4823e194ef29693
Alex <alexpark@pursuit.org>
2020-06-05 16:22:37 -0400

    Update deps and interchange map

--- map-constants.js
+++ map-constants.js
@@ -2,7 +2,7 @@ const maps = {
   woods: 'https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/0/06/WoodsMapKeySpawnsExits.jpg?version=be3e778769d3994b4b457b6be5e6e615',
   customs: 'https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/c/c8/Customs_Nuxx_20190106_1.2.png?version=a3b44edf49616eaad2736c6523c977b0',
   shoreline: 'https://preview.redd.it/1cflhynure641.jpg?width=3840&format=pjpg&auto=webp&s=9457d811e71ed10d833203f8ce70cea4b13d2ba1',
-  interchange: 'https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/0/06/InterchangeMapLorathor.jpg?version=2880f134180975c58d5c4dcef4327ef4',
+  interchange: 'https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/e/e5/InterchangeMap_Updated_4.24.2020.png?version=c1114bd10889074ca8c8d85e3d1fb04b',
   factory: 'https://external-preview.redd.it/tPt0xrxbfSR4aN4E7p8jGLa4Cc2druBnKZ0tbHjpD3I.png?auto=webp&s=f16e113034c7e6b38b59eb102d12ba8364987d10',
   labs: 'https://i.imgur.com/n1r4Ico.png',
   reserve: 'https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/9/9f/Keys_and_doors_v3.png?version=2cb6d8d74e6f3a52b8af30ca93bc86f5',
